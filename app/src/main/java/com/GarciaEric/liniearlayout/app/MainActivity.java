1d295f008dfeb5630ca4284f1ba93bae00749916
eng618 <eng618@fullsail.edu>
2014-05-09 23:01:03 -0400

    Completed all logic

--- app/src/main/java/com/GarciaEric/liniearlayout/app/MainActivity.java
+++ app/src/main/java/com/GarciaEric/liniearlayout/app/MainActivity.java
@@ -7,18 +7,34 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
     private static final String LOGTAG = "MainActivity";
     // Variables
+    private String nameString;
+    private String coffeeString;
     private boolean takesCream;
     private boolean takesSugar;
+    private boolean singleDouble;
     private int numSugar;
+    private ArrayList<String> returnStrings = new ArrayList<String>();
+    private String finalString;
 
     // Fields & Buttons
-    private Button orderBtn;
+    private EditText orderName;
+    private EditText coffeeName;
+    private RadioButton rbSingle;
+    private CheckBox creamCheck;
+    private CheckBox sugarCheck;
+    private EditText numberOfSugars;
+    public Button orderBtn;
     private TextView resultsText;
 
 
@@ -48,15 +64,110 @@ public class MainActivity extends Activity {
                 // Log message
                 Log.d(LOGTAG, "onClick entered");
 
-                // Obtain TextView
+                // Obtain fields
+                orderName = (EditText)findViewById(R.id.yourNameTextField);
+                coffeeName = (EditText)findViewById(R.id.coffeeNameTextField);
+                rbSingle = (RadioButton)findViewById(R.id.singleButton);
+                creamCheck = (CheckBox)findViewById(R.id.creamerCheck);
+                sugarCheck = (CheckBox)findViewById(R.id.sugarCheck);
+                numberOfSugars = (EditText)findViewById(R.id.numSugarTextField);
+
                 resultsText = (TextView)findViewById(R.id.resultsTextView);
-                // Set TextView text
-                resultsText.setText("values");
+
+                // Log message
+                Log.d(LOGTAG, "Fields obtained");
+
+                // Clear arrayList
+                returnStrings.clear();
+
+                // Set values
+                nameString = orderName.getText().toString();
+                coffeeString = coffeeName.getText().toString();
+                numSugar = Integer.parseInt(numberOfSugars.getText().toString());
+
+                // Log message
+                Log.d(LOGTAG, "Set values");
+
+                //Start appending strings to string array
+                returnStrings.add("Order ready for: ");
+                returnStrings.add(nameString);
+                returnStrings.add(". Your ");
+                returnStrings.add(coffeeString);
+                returnStrings.add(" is ready. ");
+
+                // Log message
+                Log.d(LOGTAG, "Started to append strings");
+
+                // Check radio buttons
+                // Is a single shoot
+                singleDouble = rbSingle.isSelected();
+
+                // Log message
+                Log.d(LOGTAG, "Checked radial buttons");
+
+                // Check checkboxes
+                if (creamCheck.isChecked() && sugarCheck.isChecked()){
+                    // Set bool's to true
+                    takesCream = true;
+                    takesSugar = true;
+                    displayOrder();
+                    return;
+                }else if (creamCheck.isChecked()){
+                    // Set bool to true
+                    takesCream = true;
+                    takesSugar = false;
+                    displayOrder();
+                    return;
+                }else if (sugarCheck.isChecked()){
+                    // Set bool to true
+                    takesSugar = true;
+                    takesCream = false;
+                    displayOrder();
+                    return;
+                }
+
+                // Log message
+                Log.d(LOGTAG, "Checked checkboxes");
+
+                displayOrder();
             }
         });
 
 
 
+    }
+
+    public void displayOrder(){
+        // Log message
+        Log.d(LOGTAG, "displayOrder entered");
+
+        // Clear previous orders
+        resultsText.setText("");
+
+        if (singleDouble){
+            returnStrings.add("It is a single shot");
+        }else {
+            returnStrings.add("It is a double shot");
+        }
+
+        if (takesCream && takesSugar){
+            returnStrings.add(" and has cream with " + numSugar + " scoops of sugar. Enjoy!!");
+        }else if (takesCream){
+            returnStrings.add(" and only has cream. Enjoy!!");
+        }else if (takesSugar){
+            returnStrings.add(" and is black with " + numSugar + " scoops of sugar. Enjoy!!");
+        }else {
+            returnStrings.add(" ENJOY!!");
+        }
+
+        // Loop through ArrayList to for finalString
+        for (int i=0; i < returnStrings.size(); i++){
+            finalString = finalString + returnStrings.get(i);
+        }
+
+
+        // Set TextView text to display
+        resultsText.setText(finalString);
     }
 
     @Override
